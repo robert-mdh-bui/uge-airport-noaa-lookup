c33335d8d18872ec2c6d7cc70963ff22fc246841
Robert Duc Bui <robertducbui@gmail.com>
2022-07-18 15:49:33 -0500

    Manual import of app.py and assets from previous repo

---
+++ app.py
@@ -0,0 +1,42 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+# Dash init
+app = dash.Dash(__name__)
+
+# Flask init
+server = app.server
+
+# App layout
+app.layout = html.Div(
+    [
+        html.Iframe(
+            id="my-output",
+            src="assets/ORD.html",
+            style={'height': '495px',
+                   'width': '100%'}
+        ),
+        dcc.Input(
+            id='textinput',
+            placeholder='Enter 3-letter IATA code',
+            type='text',
+            value='ORD'
+        )
+    ]
+)
+
+
+# Callback decorator for updating func
+@app.callback(
+    Output('my-output', 'src'),
+    Input('textinput', 'value'),
+    prevent_initial_call=True
+)
+def update_output_div(input_value):
+    return f'assets/{input_value}.html'
+
+# Check
+if __name__ == '__main__':
+    app.run_server(debug=True)
