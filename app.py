39601e9ee55e24c1301c0a2132a6c306047b08b9
Robert Duc Bui <robertducbui@gmail.com>
2022-07-18 23:13:00 -0500

    Added information to app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import dash
-import dash_core_components as dcc
+from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 
@@ -12,25 +12,91 @@ server = app.server
 # App layout
 app.layout = html.Div(
     [
-        html.Iframe(
-            id="my-output",
-            src="assets/ORD.html",
-            style={'height': '900px',
-                   'width': '100%'}
+
+        html.H2(
+            children="Web App: Mapping 5 nearest NOAA weather stations to US airports",
+            style={
+                'textAlign': 'center',
+                'font_family': 'SourceSansProSemiBold'
+            }
         ),
+
+        dcc.Markdown(
+            children='Created by [Robert Bui](https://github.com/robert-mdh-bui/uge-airport-noaa-lookup). \
+            Licensed under [GNU General Public License 3.0](https://www.gnu.org/licenses/gpl-3.0.en.html)',
+            style={
+                'display': 'flex',
+                'justifyContent': 'center'
+            }
+        ),
+        dcc.Markdown(
+            children='''
+            This web app is derived from a project originally created for [United Ground Express](https://unitedgroundexpress.com/) \
+            operations planning.
+            Data sources consist of publicly available information from [IATA](https://www.iata.org/en/publications/directories/code-search/)\
+            and [NOAA](https://www.ncei.noaa.gov/products/land-based-station/global-historical-climatology-network-daily).
+            ''',
+            style={
+                'display': 'flex',
+                'justifyContent': 'center'
+            }
+        ),
+        dcc.Markdown(
+            children='''
+            Tools used:
+            -   `folium`: Python wrapper for `JS.leaflet` to create interactive GIS maps.
+            -   `dash`: Python package by `plotly` to implement interactive visualization.
+            -   Miscellaneous: `pandas`, `airportsdata`, `haversine`.
+            -   Deployed on Salesforce Heroku, powered by `dash` and `gunicorn.`''',
+            style={
+                'display': 'flex',
+                'justifyContent': 'center'
+            }
+        ),
+
         html.Div(
-            children=[
-                    dcc.Input(
-                        id='textinput',
-                        placeholder='Enter 3-letter IATA code',
-                        type='text',
-                        value='ORD'
-                    )
-            ],
-            style=dict(
-                display='flex',
-                justifyContent='center'
-            )
+            [
+
+                # HTML div for input text box
+                html.Div(
+                    [
+                        html.Div(
+                            children="Input 3-Letter IATA Code (ALL CAPS - US airports only): ",
+                            style={
+                                'textAlign': 'center'
+                            }
+                        ),
+                        dcc.Input(
+                            id='textinput',
+                            type='text',
+                            value='ORD'
+                        )
+                    ],
+                    style={
+                        'display':'flex',
+                        'justifyContent':'center'
+                    }
+                ),
+
+                # HTML div for output map
+                html.Div(
+                    [
+                        html.Iframe(
+                            id="my-output",
+                            src="assets/ORD.html",
+                            style={
+                                'height': '800px',
+                                'width': '90%',
+                                'margin-top': '20px',
+                                'margin-left': '5%',
+                                'margin-right': '5%'
+                                #'display': 'flex',
+                                #'justifyContent': 'center'
+                            }
+                        )
+                    ]
+                ),
+            ]
         )
     ]
 )
